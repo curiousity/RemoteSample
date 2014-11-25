42bb6018e80c727697d68c1b3cc5ab235a2c54c0
curiosity <senjusforest@gmail.com>
2014-11-26 00:36:51 +0530

    a rethorical stmt

--- Sample/src/HelloWorld.java
+++ Sample/src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
-		System.out.println("Welcome Batman!");
+		System.out.println("Welcome Batman!.Ready for Action");
 
 	}
 
